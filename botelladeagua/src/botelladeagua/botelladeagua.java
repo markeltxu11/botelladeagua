3175cfccd0dab2c192e4346f7a4794b1a44933b2
markeltxu11 <marpalui1@hotmail.com>
2017-04-29 20:19:12 +0200

    Update botelladeagua.java

--- botelladeagua/src/botelladeagua/botelladeagua.java
+++ botelladeagua/src/botelladeagua/botelladeagua.java
@@ -5,6 +5,8 @@ public class botelladeagua {
 	public static void main(String[] args) {
 	
 			System.out.println("Soy una botella de agua, con minerales cambiados");
+			System.out.println("Hola soy una silla");
+			
 	}
 
 }
